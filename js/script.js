25fbdd0f5ce71e0c0f82a3fd08476cdb54084de6
Serhat KAZANCI <109909204+SerhatKazanci@users.noreply.github.com>
2022-10-08 18:16:31 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,83 @@
+
+        $(function () {
+
+
+            $("#img1").mouseover(function () {
+
+                $("#div1").fadeIn(2000).css("background-image", "url(img/antikkent.webp");
+
+            });
+            $("#img1").mouseleave(function () {
+
+                $("#div1").css("display", "none");
+
+            });
+
+
+            $("#img2").mouseover(function () {
+
+                $("#div1").fadeIn(2000).css("background-image", "url(img/foca.jpg)");
+
+            });
+            $("#img2").mouseleave(function () {
+
+                $("#div1").css("display", "none");
+
+            });
+
+            $("#img3").mouseover(function () {
+
+                $("#div1").fadeIn(2000).css("background-image", "url(img/inciraltı.jpg)");
+
+            });
+            $("#img3").mouseleave(function () {
+
+                $("#div1").css("display", "none");
+
+            });
+
+
+            $("#img4").mouseover(function () {
+
+                $("#div1").fadeIn(2000).css("background-image", "url(img/saatkulesi.jpg)");
+
+            });
+            $("#img4").mouseleave(function () {
+
+                $("#div1").css("display", "none");
+
+            });
+
+
+            $("#img5").mouseover(function () {
+
+                $("#div1").fadeIn(2000).css("background-image", "url(img/sirince.jpg)");
+
+            });
+            $("#img5").mouseleave(function () {
+
+                $("#div1").css("display", "none");
+
+            });
+
+            $("#img6").mouseover(function () {
+
+                $("#div1").fadeIn(2000).css("background-image", "url(img/tarihiasansör.jpg)");
+
+            });
+            $("#img6").mouseleave(function () {
+
+                $("#div1").css("display", "none");
+
+            });
+
+
+
+        });
+
+
+
+
+
+
+    
